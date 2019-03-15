56c223d1b83a29a054dd6e4521f55f1b951fdeeb
Brian Ho <YWHo@users.noreply.github.com>
2019-03-16 12:13:27 +1300

    use webpack-html-plugin to create index.html

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,7 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: "./src/scripts/app.js", //path relative to this file
   output: {
     filename: "./dist/app.bundle.js" //path relative to this file
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'My Awesome application',
+      myPageHeader: 'Hello World',
+      template: './src/index.html',
+      filename: './dist/index.html' // relative to root of the application
+    })
+  ]
 }
